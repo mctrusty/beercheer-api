74bcdd2cf94dda1b5dc05c01f6a4e80c41d36dc6
Michael J. Cox <mike.jon.cox@gmail.com>
2016-08-20 06:55:28 -0600

    changed to include store name

--- db/create-beerdeals-view.sql
+++ db/create-beerdeals-view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW beer_deals AS
-SELECT beer, brewer, pkg.qty, pkg.size, pkg.container, store.name, "googleId", price.price FROM
+SELECT beer, brewer, pkg.qty, pkg.size, pkg.container, store.name as store, "googleId", price.price FROM
 beers join price on beers.id = price."beerId"
 join store on store.id = price."storeId"
 join pkg on pkg.id = price."pkgId";
